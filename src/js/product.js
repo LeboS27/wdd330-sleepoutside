9c7297b7032ddd79a9b128d0840dd861c204dc7c
Lebohang Sebata <lebohangsebata20@gmail.com>
2026-03-08 15:42:24 +0200

    week 1 done

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,18 @@
-import { setLocalStorage } from "./utils.mjs";
+// product.js (partial)
+import { getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 
 const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // Get existing cart or initialize empty array
+  let cartContents = getLocalStorage("so-cart") || [];
+  // Add new product
+  cartContents.push(product);
+  // Save updated array
+  setLocalStorage("so-cart", cartContents);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
